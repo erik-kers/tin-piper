ed0f9034c881dcd8b2750067b75246af8d3a6634
Erik Kers <erik.kers@innofactor.com>
2020-10-11 13:21:51 +0200

    Refactor data
    * Add real song 'British Grenadiers'
    * Change name from fingerings-object to fingeringTable
    * Change from using [note]+ distinguishing second octave note to
       setting second property 'octave' on notes.
    * Following the above, remove all [note]+ notes from fingeringTable and replace with
       first- and secondOctave properties

--- src/data.js
+++ src/data.js
@@ -1,74 +1,204 @@
 export const songs = [
   {
     id: "1",
-    title: "Song A",
-    author: "Rachmaninoff",
-    description: "Test #1",
+    title: "The British Grenadiers",
+    author: "Traditional marching song",
+    description:
+      "\"The British Grenadiers\" is a traditional marching song of British, Australian and Canadian military units whose badge of identification features a grenade, the tune of which dates from the 17th century. It is the Regimental Quick March of the Royal Artillery (since 1716), Corps of Royal Engineers (since 1787), the Honourable Artillery Company (since 1716), Grenadier Guards 'The First (later 'Grenadier') Regiment of Foot Guards' (since 1763), and the Royal Regiment of Fusiliers (since 1763). It is also an authorised march of the Royal Australian Artillery, The Royal Gibraltar Regiment, The Royal Regiment of Canadian Artillery, The Royal Regiment of Canada, The Princess Louise Fusiliers, and The 5th Canadian Mounted Rifles. The standard orchestration for the military band was approved in 1762, when the Royal Artillery Band (initiated in 1557) became recognised officially,[1] and for all other 'grenade' regiments in 1763, when the remaining unofficial bands gained official status.",
     keySignature: "D",
     time: "4/4",
     systems: [
       {
         bars: [
           {
-            notes: [{ note: "D" }, { note: "F-sharp" }],
+            notes: [{ note: "D", octave: 1 }],
           },
           {
             notes: [
-              { note: "E" },
-              { note: "F-sharp" },
-              { note: "G" },
-              { note: "A" },
+              { note: "G", octave: 1 },
+              { note: "D", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "A", octave: 1 },
             ],
           },
           {
-            notes: [{ note: "B" }, { note: "C" }, { note: "C-sharp" }],
+            notes: [
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "C", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "D", octave: 2 },
+              { note: "G", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "F#", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "G", octave: 1 },
+              { note: "D", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "G", octave: 1 },
+              { note: "D", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "A", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "C", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "D", octave: 2 },
+              { note: "G", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "F#", octave: 1 },
+            ],
+          },
+          {
+            notes: [{ note: "G", octave: 1 }],
           },
         ],
       },
       {
         bars: [
           {
-            notes: [{ note: "D" }, { note: "D" }, { note: "D" }],
-          },
-          {
             notes: [
-              { note: "D+" },
-              { note: "E+" },
-              { note: "C-sharp" },
-              { note: "C-sharp" },
+              { note: "B", octave: 1 },
+              { note: "C", octave: 1 },
             ],
           },
           {
             notes: [
-              { note: "C" },
-              { note: "C" },
-              { note: "D" },
-              { note: "D" },
-              { note: "C" },
+              { note: "D", octave: 2 },
+              { note: "E", octave: 2 },
+              { note: "D", octave: 2 },
+              { note: "C", octave: 1 },
             ],
+          },
+          {
+            notes: [
+              { note: "B", octave: 1 },
+              { note: "C", octave: 1 },
+              { note: "D", octave: 2 },
+              { note: "D", octave: 2 },
+            ],
+          },
+          {
+            notes: [
+              { note: "E", octave: 2 },
+              { note: "E", octave: 2 },
+              { note: "D", octave: 2 },
+              { note: "C", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "G", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "F#", octave: 1 },
+              { note: "D", octave: 1 },
+              { note: "D", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "G", octave: 1 },
+              { note: "F#", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "A", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "C", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "C", octave: 1 },
+            ],
+          },
+          {
+            notes: [
+              { note: "D", octave: 2 },
+              { note: "G", octave: 1 },
+              { note: "B", octave: 1 },
+              { note: "A", octave: 1 },
+              { note: "G", octave: 1 },
+              { note: "F#", octave: 1 },
+            ],
+          },
+          {
+            notes: [{ note: "G", octave: 1 }],
           },
         ],
       },
     ],
   },
 ];
-export const fingerings = {
+export const fingeringTables = {
   D: [
-    { note: "D", fingering: [1, 1, 1, 1, 1, 1] },
-    { note: "E", fingering: [1, 1, 1, 1, 1, 0] },
-    { note: "F-sharp", fingering: [1, 1, 1, 1, 0, 0] },
-    { note: "G", fingering: [1, 1, 1, 0, 0, 0] },
-    { note: "A", fingering: [1, 1, 0, 0, 0, 0] },
-    { note: "B", fingering: [1, 0, 0, 0, 0, 0] },
-    { note: "C", fingering: [0, 1, 1, 0, 0, 0] },
-    { note: "C-sharp", fingering: [0, 0, 0, 0, 0, 0] },
-    { note: "D+", fingering: [0, 1, 1, 1, 1, 1] },
-    { note: "E+", fingering: [1, 1, 1, 1, 1, 0] },
-    { note: "F-sharp+", fingering: [1, 1, 1, 1, 0, 0] },
-    { note: "G+", fingering: [1, 1, 1, 0, 0, 0] },
-    { note: "A+", fingering: [1, 1, 0, 0, 0, 0] },
-    { note: "B", fingering: [1, 0, 0, 0, 0, 0] },
-    { note: "C+", fingering: [0, 1, 1, 1, 0, 0] },
-    { note: "C-sharp+", fingering: [0, 0, 0, 0, 0, 0] },
+    {
+      note: "D",
+      firstOctave: [1, 1, 1, 1, 1, 1],
+      secondOctave: [0, 1, 1, 1, 1, 1],
+    },
+    {
+      note: "E",
+      firstOctave: [1, 1, 1, 1, 1, 0],
+      secondOctave: [1, 1, 1, 1, 1, 0],
+    },
+    {
+      note: "F#",
+      firstOctave: [1, 1, 1, 1, 0, 0],
+      secondOctave: [1, 1, 1, 1, 0, 0],
+    },
+    {
+      note: "G",
+      firstOctave: [1, 1, 1, 0, 0, 0],
+      secondOctave: [1, 1, 1, 0, 0, 0],
+    },
+    {
+      note: "A",
+      firstOctave: [1, 1, 0, 0, 0, 0],
+      secondOctave: [1, 1, 0, 0, 0, 0],
+    },
+    {
+      note: "B",
+      firstOctave: [1, 0, 0, 0, 0, 0],
+      secondOctave: [1, 0, 0, 0, 0, 0],
+    },
+    {
+      note: "C",
+      firstOctave: [0, 1, 1, 0, 0, 0],
+      secondOctave: [0, 1, 1, 1, 0, 0],
+    },
+    {
+      note: "C#",
+      firstOctave: [0, 0, 0, 0, 0, 0],
+      secondOctave: [0, 0, 0, 0, 0, 0],
+    },
   ],
 };
