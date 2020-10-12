7a274ea606fa2ada1781158e4ee42a80d8e46227
Erik Kers <erik.kers@innofactor.com>
2020-10-12 10:08:51 +0200

    Move song metadata to SongInformation

--- src/components/song/song.component.jsx
+++ src/components/song/song.component.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
 import PropTypes from "prop-types";
-import System from "../system/system.component";
+import Tablature from "../tablature/tablature.component";
+import SongInformation from "../song-information/song-information.component";
 
 const SongContainer = styled.section`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  height: 100%;
-  width: 100%;
+  height: auto;
+  width: 60vw;
+  margin: 50px;
 `;
 
 const Song = ({ title, author, description, keySignature, time, systems }) => {
   return (
     <SongContainer>
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{description}</p>
-      <span>
-        {keySignature} - {time}
-      </span>
-      {systems.map((system) => {
-        const { bars } = system;
-        return <System bars={bars} key={uuidv4} />;
-      })}
+      <SongInformation
+        title={title}
+        author={author}
+        keySignature={keySignature}
+        time={time}
+        description={description}
+      />
+      <Tablature systems={systems} />
     </SongContainer>
   );
 };
@@ -35,7 +33,7 @@ Song.propTypes = {
   description: PropTypes.string.isRequired,
   keySignature: PropTypes.string.isRequired,
   time: PropTypes.string.isRequired,
-  systems: PropTypes.array.isRequired,
+  systems: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Song;
